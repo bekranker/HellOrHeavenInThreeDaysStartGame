03b322feb85a78b9ef3a682fd3d825d97021bb0e
Bekir <aqwwwe73@gmail.com>
2024-12-14 00:23:07 +0300

    holy book started

---
+++ Scripts/HolyBook/Sin.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Sin", menuName = "Scriptable Objects/Sin")]
+public class Sin : ScriptableObject
+{
+	//the line;
+	public string SinLine;
+	//maks is 7;
+	public int Order;
+}

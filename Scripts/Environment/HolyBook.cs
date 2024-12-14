a53d2ee58283831a233003062a44d5b60273dd04
Bekir <aqwwwe73@gmail.com>
2024-12-14 22:35:17 +0300

    before god

--- Scripts/Environment/HolyBook.cs
+++ Scripts/Environment/HolyBook.cs
@@ -5,6 +5,7 @@ public class HolyBook : MonoBehaviour, Interaction
 {
     public event Action OnClick;
     [SerializeField] private GameObject _HolyBook;
+
     public void OnClickEvent()
     {
         _HolyBook.SetActive(true);

aa8083af3c99651ffb3ec331fef971f6d9543022
Bekir <aqwwwe73@gmail.com>
2024-12-15 07:23:53 +0300

    ToolTip added but there is has offset bug

--- Icons.cs
+++ Icons.cs
@@ -6,15 +6,14 @@ public class Icons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     [SerializeField] private string _toolTip;
     //we will spawn that prefab and drag it where we want
     [SerializeField] private GameObject _HoldingIcon;
-
+    [SerializeField] private ToolTip _ToolTip;
     public void OnPointerEnter(PointerEventData eventData)
     {
-
-        throw new System.NotImplementedException();
+        _ToolTip.Show(_toolTip);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        _ToolTip.UnShow();
     }
 }
---
+++ ToolTip.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class ToolTip : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _CanvasGroup;
+    [SerializeField] private float _Speed;
+    [SerializeField] private TMP_Text _toolText;
+
+
+
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos - (transform.localScale / 2);
+    }
+
+    public void Show(string tip)
+    {
+        DOTween.Kill(_CanvasGroup);
+        _CanvasGroup.DOFade(1, _Speed);
+        _toolText.text = tip;
+    }
+    public void UnShow()
+    {
+        DOTween.Kill(_CanvasGroup);
+        _CanvasGroup.DOFade(0, _Speed);
+    }
+}
